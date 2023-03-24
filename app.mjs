2abc37096417ab412dbc7c74eea8435dc0aa8da5
Willis1118 <nm3607@nyu.edu>
2023-03-24 18:53:04 -0400

    minor change

--- app.mjs
+++ app.mjs
@@ -2,6 +2,8 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
 
+import './schemas/db.mjs'
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
