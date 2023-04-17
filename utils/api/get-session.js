f627c9f150cc57756046b6c4a5e0b21d426b914f
Willis1118 <nm3607@nyu.edu>
2023-04-17 09:55:01 -0400

    fix SQLite

--- utils/api/get-session.js
+++ utils/api/get-session.js
@@ -1,11 +1,15 @@
+import path from 'path';
 import nextSession from "next-session";
+
 import { expressSession, promisifyStore } from "next-session/lib/compat";
 let SQLiteStore = require("connect-sqlite3")(expressSession);
+
+const __path = path.join(process.cwd(), 'tmp');
 
 export const getSession = nextSession({
   name: "WIB_SESSION",
   store: promisifyStore(
-    new SQLiteStore({ dir: "./tmp/", table: "wiberSessions" })
+    new SQLiteStore({ dir: __path, table: "wiberSessions" })
   ),
   cookie: {
     secure: true,
